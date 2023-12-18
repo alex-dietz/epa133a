6a2efce715f215e8030a84e678dd829a33a95873
Alex Dietz <62934780+alex-dietz@users.noreply.github.com>
2023-12-18 17:10:23 +0100

    copy .js file from assignment 2 to 3

--- EPA133a-Gxx-A3/model/ContinuousSpace/simple_continuous_canvas.js
+++ EPA133a-Gxx-A3/model/ContinuousSpace/simple_continuous_canvas.js
@@ -1,111 +1,115 @@
-var ContinuousVisualization = function(height, width, context) {
-	var height = height;
-	var width = width;
-	var context = context;
+// Function to dynamically load jQuery
+function loadJQuery(callback) {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"; // URL to jQuery library
+  script.onload = callback; // Callback after jQuery has loaded
+  document.head.appendChild(script);
+}
 
-	this.draw = function(objects) {
-		for (var i in objects) {
-			var p = objects[i];
-			if (p.Shape == "rect")
-				this.drawRectangle(p.x, p.y, p.w, p.h, p.Color, p.Filled, p.Text, p.Text_color);
-			if (p.Shape == "circle")
-				this.drawCircle(p.x, p.y, p.r, p.Color, p.Filled, p.Text, p.Text_color);
-		};
-	};
+var ContinuousVisualization = function (height, width, context) {
+  var height = height;
+  var width = width;
+  var context = context;
 
-	this.drawCircle = function(x, y, radius, color, fill, text, text_color) {
+  this.draw = function (objects) {
+    for (var i in objects) {
+      var p = objects[i];
+      if (p.Shape == "rect") this.drawRectangle(p.x, p.y, p.w, p.h, p.Color, p.Filled, p.Text, p.Text_color);
+      if (p.Shape == "circle") this.drawCircle(p.x, p.y, p.r, p.Color, p.Filled, p.Text, p.Text_color);
+    }
+  };
 
-		var cx = x * width;
-		var cy = y * height;
-		var r = radius;
+  this.drawCircle = function (x, y, radius, color, fill, text, text_color) {
+    var cx = x * width;
+    var cy = y * height;
+    var r = radius;
 
-		context.beginPath();
-		context.arc(cx, cy, r, 0, Math.PI * 2, false);
-		context.closePath();
+    context.beginPath();
+    context.arc(cx, cy, r, 0, Math.PI * 2, false);
+    context.closePath();
 
-		context.strokeStyle = color;
-		context.stroke();
+    context.strokeStyle = color;
+    context.stroke();
 
-		if (fill) {
-			context.fillStyle = color;
-			context.fill();
-		}
+    if (fill) {
+      context.fillStyle = color;
+      context.fill();
+    }
 
-		// This part draws the text inside the Circle
-        if (text !== undefined) {
-                context.fillStyle = text_color;
-                context.textAlign = 'center';
-                context.textBaseline= 'middle';
-                context.fillText(text, cx, cy);
-        }
+    // This part draws the text inside the Circle
+    if (text !== undefined) {
+      context.fillStyle = text_color;
+      context.textAlign = "center";
+      context.textBaseline = "middle";
+      context.fillText(text, cx, cy);
+    }
+  };
 
-	};
+  this.drawRectangle = function (x, y, w, h, color, fill, text, text_color) {
+    var cx = x * width;
+    var cy = y * height;
+    var dx = w;
+    var dy = h;
 
-	this.drawRectangle = function(x, y, w, h, color, fill, text, text_color) {
+    // Keep the drawing centered:
+    var x0 = cx - dx * 0.5;
+    var y0 = cy - dy * 0.5;
 
-		var cx = x * width;
-		var cy = y * height;
-		var dx = w;
-		var dy = h;
+    context.beginPath();
+    context.strokeStyle = color;
+    context.fillStyle = color;
 
-		// Keep the drawing centered:
-		var x0 = cx - dx * 0.5;
-		var y0 = cy - dy * 0.5;
+    if (fill) context.fillRect(x0, y0, dx, dy);
+    else context.strokeRect(x0, y0, dx, dy);
 
-		context.beginPath();
-		context.strokeStyle = color;
-		context.fillStyle = color;
+    // This part draws the text inside the Rectangle
+    if (text !== undefined) {
+      context.fillStyle = text_color;
+      context.textAlign = "center";
+      context.textBaseline = "middle";
+      context.fillText(text, cx, cy);
+    }
+  };
 
-		if (fill)
-			context.fillRect(x0, y0, dx, dy);
-		else
-			context.strokeRect(x0, y0, dx, dy);
-
-		// This part draws the text inside the Rectangle
-        if (text !== undefined) {
-                context.fillStyle = text_color;
-                context.textAlign = 'center';
-                context.textBaseline= 'middle';
-                context.fillText(text, cx, cy);
-        }
-	};
-
-	this.resetCanvas = function() {
-		context.clearRect(0, 0, height, width);
-		context.beginPath();
-	};
+  this.resetCanvas = function () {
+    context.clearRect(0, 0, height, width);
+    context.beginPath();
+  };
 };
 
-var Simple_Continuous_Module = function(canvas_width, canvas_height) {
-	// Create the element
-	// ------------------
+var Simple_Continuous_Module = function (canvas_width, canvas_height) {
+  var canvasDraw;
+  // Create the element
+  // ------------------
+  function initialize() {
+    // Create the tag:
+    var canvas_tag = "<canvas width='" + canvas_width + "' height='" + canvas_height + "' ";
+    canvas_tag += "style='border:1px dotted'></canvas>";
 
-	// Create the tag:
-	var canvas_tag = "<canvas width='" + canvas_width + "' height='" + canvas_height + "' ";
-	canvas_tag += "style='border:1px dotted'></canvas>";
-	
-	var parent_div_tag = '<div style="height:' + canvas_height + 'px;" class="world-grid-parent" target="_sim"></div>'
-	
-	// Append it to body:
-	var canvas = $(canvas_tag)[0];
-	
-	var parent = $(parent_div_tag)[0];
-	
-	//$("body").append(canvas);
-	$("#elements").append(parent);
-	parent.append(canvas);
+    var parent_div_tag = '<div style="height:' + canvas_height + 'px;" class="world-grid-parent" target="_sim"></div>';
 
-	// Create the context and the drawing controller:
-	var context = canvas.getContext("2d");
-	var canvasDraw = new ContinuousVisualization(canvas_width, canvas_height, context);
+    // Append it to body:
+    var canvas = $(canvas_tag)[0];
 
-	this.render = function(data) {
-		canvasDraw.resetCanvas();
-		canvasDraw.draw(data);
-	};
+    var parent = $(parent_div_tag)[0];
 
-	this.reset = function() {
-		canvasDraw.resetCanvas();
-	};
+    //$("body").append(canvas);
+    $("#elements").append(parent);
+    parent.append(canvas);
 
+    // Create the context and the drawing controller:
+    var context = canvas.getContext("2d");
+    canvasDraw = new ContinuousVisualization(canvas_width, canvas_height, context);
+  }
+  loadJQuery(initialize);
+
+  this.render = function (data) {
+    canvasDraw.resetCanvas();
+    canvasDraw.draw(data);
+  };
+
+  this.reset = function () {
+    canvasDraw.resetCanvas();
+  };
 };
